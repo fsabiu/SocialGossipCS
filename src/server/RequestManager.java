fc93ca8d4763d36e3353f50dac77cc4ebaf7d128
fsabiu <fsabiu@gmail.com>
2018-06-24 11:03:37 +0200

    Register operation implemented

--- src/server/RequestManager.java
+++ src/server/RequestManager.java
@@ -6,6 +6,7 @@ import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.UTFDataFormatException;
 import java.net.Socket;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -14,6 +15,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import communication.Operation;
+import communication.RequestMessage;
 import communication.ResponseMessage;
 
 import org.json.*;
@@ -82,14 +84,14 @@ public class RequestManager implements Runnable {
 					System.out.println("Connection closed by client");
 					break;
 				}
-				catch (IOException e) {
-					e.printStackTrace();
+				catch (IOException e1) {
+					System.out.println("IO Exception while reading client message");
 				}
 			}
 			
 		} 
 		catch (IOException e1) {
-			e1.printStackTrace();
+			System.out.println("IO Exception while nstantiating client connection");
 		}
 		
 		//Closing 
@@ -110,29 +112,49 @@ public class RequestManager implements Runnable {
 	private void executeRequest(String request, DataOutputStream out) {
 		// TODO Auto-generated method stub
 		try {
-			JSONParser parser= new JSONParser();
+			//Creation reply message
+			ResponseMessage reply= new ResponseMessage();
 			
 			//Parsing JSON message
-			JSONObject message= (JSONObject) parser.parse(request);
-			//Getting operation from JSONObject
-			Operation op = (Operation) message.get("OPERATION");
+			JSONParser parser= new JSONParser();
+			RequestMessage message= (RequestMessage) parser.parse(request);
 			
-			//Creation reply message
-			/**
-			 * ResponseMessage reply= new ResponseMessage(); 
-			 * Proposta modifica costruttore, farlo senza operazione per
-			 * poterlo scrivere nello switch a seconda dei casi
-			 */
+			//Getting operation from JSONObject
+			Operation op = (Operation) message.getParameter("OPERATION");
+			
+			//Validating operation field
+			if(op==null) {
+				System.out.println("Invalid operation type recived");
+				reply.setParameters("OPERATION:ERR","BODY:Invalid request received");
+			}
+			
+			
 			switch(op) {
 				case REGISTER:
-					//Extracting relevant fields
-					String username= (String) message.get("USERNAME");
-					String password= (String) message.get("PASSWORD");
-					String language= (String) message.get("LANGUAGE");
-					
-					User new_user= new User(username, password, language);//Catturare IllegalArgument
-					usersbyname.put(username, new_user);//Vedere se esiste già
-					network.addVertex(new_user);//
+					try {
+						//Extracting relevant fields
+						String username= (String) message.getParameter("USERNAME");
+						String password= (String) message.getParameter("PASSWORD");
+						String language= (String) message.getParameter("LANGUAGE");
+						
+						//Validating parameters
+						if(username==null || password==null || language== null) {
+							throw new IllegalArgumentException();
+						}
+						
+						//If user already exists
+						if(usersbyname.containsKey(username)) {
+							reply.setParameters("OPERATION:ERR","BODY:Username already exists");
+						}
+						
+						//Adding user to data structures
+						User new_user= new User(username, password, language);
+						usersbyname.put(username, new_user);
+						network.addVertex(new_user);
+						
+					}catch(IllegalArgumentException e) {
+						System.out.println("Invalid request parameters received by the client");
+					}
 					break;
 				case UNREGISTER:
 					break;
@@ -165,8 +187,9 @@ public class RequestManager implements Runnable {
 				default:
 					break;
 			}
-		}
-		catch(ParseException | NullPointerException e) {
+		} catch(ParseException e) {
+			
+		} catch(IllegalArgumentException e) {
 			
 		}
 	}
